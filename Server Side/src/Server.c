900c4ebe2fe0718873de409ed2f44978f6095f91
Isaac <isaacchircop@gmail.com>
2014-06-24 22:06:46 +0200

    Does not work correctly - added pipes

--- Server Side/src/Server.c
+++ Server Side/src/Server.c
@@ -70,6 +70,8 @@ int main(void) {
 	unsigned int addressLength = sizeof(struct sockaddr_in);
 
 	int newSocket;
+	int parentRead[2];
+	int childRead[2];
 
 	do {
 
@@ -83,14 +85,68 @@ int main(void) {
 
 		}
 
+		// Create Pipe for parent child communication
+
+		int pipeErr = pipe(parentRead);
+
+		if(pipeErr < 0){
+
+			printf("%s\n", strerror(errno));
+			exit(-1);
+
+		}
+
+		dup2(parentRead, childRead);
+
 		// Create new process:	Child Process handles client requests
 		//						Parent Process handles other connections
 
 		pid = fork();
 
+		if (pid > 0) {
+
+			close (childRead[0]);		// Close read fd for childRead pipe
+			close (parentRead[1]);		// Close write fd for parentRead pipe
+
+			fcntl (childRead[0], F_SETFL, O_NONBLOCK);
+			fcntl (parentRead[1], F_SETFL, O_NONBLOCK);
+
+			// Read any updates from children
+
+			off_t fileOffset;
+			int offsetRead = read (parentRead[0], &fileOffset, sizeof(fileOffset));
+
+			if (offsetRead > 0) {
+
+				printf ("Received Update\n");
+
+				// Send update to all children
+
+				int count;
+				read (parentRead[0], &count, sizeof(count));
+
+				char *buff = (char *)malloc(sizeof(count));
+				read (parentRead[0], &buff, count);
+
+				write (childRead[1], &fileOffset, sizeof(fileOffset));
+				write (childRead[1], &count, sizeof(count));
+				write (childRead[1], &buff, count);
+
+			}
+
+		}
+
 	} while (pid > 0);
 
 	if (pid == 0) {
+
+		close (childRead[1]);			// Close write fd for childRead pipe
+		close (parentRead[0]);			// Close read fd for parentRead pipe
+
+		fcntl (childRead[0], F_SETFL, O_NONBLOCK);
+		fcntl (parentRead[1], F_SETFL, O_NONBLOCK);
+
+		fcntl (newSocket, F_SETFL, O_NONBLOCK);
 
 		// Child Process is created whenever clients connects to server - requests for rmmap
 
@@ -148,112 +204,167 @@ int main(void) {
 
 		do {
 
-			read (newSocket, &request, sizeof(request));
+			// Check for updates from parent process
 
-			switch (request) {
+			off_t fileOffset;
+			int offsetRead = read (childRead[0], &fileOffset, sizeof(fileOffset));
 
-				case 1:
+			if (offsetRead > 0) {
 
-					// Unmap and close connection
+				printf ("Update\n");
 
-					printf ("Closing Connection with client %d...\n", getpid());
-					close (newSocket);
-					kill (getpid(), 0);		// Perform Suicide
-					break;
+				int count;
+				read (childRead[0], &count, sizeof(count));
 
-				case 2: {
+				char *buff = (char *)malloc(sizeof(count));
+				int bytesRead = read (childRead[0], &buff, count);
 
-					// **** Start of write request handling ****
+				// Send Update information to client
 
-					rsp resp;
-					char *message;
+				write (newSocket, &fileOffset, sizeof(fileOffset));
+				write (newSocket, &count, sizeof(count));
+				write (newSocket, &buff, count);
 
-					// Get details of Write Request
-					off_t write_offset;
-					read(newSocket, &write_offset, sizeof(write_offset));
+				free (buff);
 
-					int count;
-					read(newSocket, (int *)&count, sizeof(int));
+			} else {
 
-					char *buff = (char *)malloc(sizeof(char) * count);
-					read(newSocket, buff, sizeof(char)*count);
+				/// Read for client requests
 
-					// Open file with Write Access
-					int fd = open(pathname, O_WRONLY);
-					if (fd < 0) {
+				int clientRead = read (newSocket, &request, sizeof(request));
 
-						printf("%s\n", strerror(errno));
+				if (clientRead > 0) {
 
-						resp.number = -1;
-						message = "Could Not Open File";
-						strcpy (resp.message, message);
+					switch (request) {
 
-						write (newSocket, &resp, sizeof(resp));
+						case 1:
 
-						break;
+							// Unmap and close connection
+
+							printf ("Closing Connection with client %d...\n", getpid());
+							close (newSocket);
+							close (parentRead[1]);
+							close (childRead[0]);
+							kill (getpid(), 0);		// Perform Suicide
+							break;
+
+						case 2: {
+
+							// **** Start of write request handling ****
+
+							printf ("Write Request received\n");
+
+							rsp resp;
+							char *message;
+
+							// Get details of Write Request
+							off_t write_offset;
+							read(newSocket, &write_offset, sizeof(write_offset));
+
+							int count;
+							read(newSocket, (int *)&count, sizeof(int));
+
+							char *buff = (char *)malloc(sizeof(char) * count);
+							read(newSocket, buff, sizeof(char)*count);
+
+							// Open file with Write Access
+							int fd = open(pathname, O_WRONLY);
+							if (fd < 0) {
+
+								printf("%s\n", strerror(errno));
+
+								resp.number = -1;
+								message = "Could Not Open File";
+								strcpy (resp.message, message);
+
+								write (newSocket, &resp, sizeof(resp));
+
+								break;
+
+							}
+
+							// ** File Lock Handling **
+
+							struct flock savelock, unlock;
+
+							savelock.l_type = F_WRLCK;
+							savelock.l_whence = SEEK_SET;
+							savelock.l_start = offset + write_offset;
+							savelock.l_len = count;
+
+							unlock.l_type = F_UNLCK;
+							unlock.l_whence = SEEK_SET;
+							unlock.l_start = offset + write_offset;
+							unlock.l_len = count;
+
+							// Lock file segment that will be modified
+							int x = fcntl (fd, F_SETLK, &savelock);
+
+							if (x == -1) {
+
+								resp.number = -1;
+								message = "File is locked";
+								strcpy (resp.message, message);
+
+								write (newSocket, &resp, sizeof(resp));
+
+							}
+
+							// Perform write
+
+							off_t fileOffset = offset + write_offset;
+
+							lseek (fd, fileOffset, SEEK_SET);
+							int bytes = write (fd, buff, count);
+
+							// Inform parent of file update
+							write (parentRead[1], &fileOffset, sizeof(fileOffset));
+							write (parentRead[1], &count, sizeof(count));
+							write (parentRead[1], buff, count);
+
+							char c;
+							scanf ("%c", &c);
+
+							// Unlock file segment that was modified
+							int y = fcntl (fd, F_SETLK, &unlock);
+
+							if (y == -1) {
+
+								resp.number = -1;
+								message = "Could not unlock file";
+								strcpy (resp.message, message);
+
+								write (newSocket, &resp, sizeof(resp));
+
+							} else {
+
+								if (bytes == -1) {
+
+									printf ("%s\n", strerror(errno));
+
+								} else {
+
+									resp.number = bytes;
+									message = "Success";
+									strcpy (resp.message, message);
+
+									write (newSocket, &resp, sizeof(resp));
+
+								}
+
+							}
+
+							// Free buffer and close file descriptor
+							free (buff);
+							int k = close (fd);
+
+							printf ("%d\n", k);
+
+							break;
+
+						}
 
 					}
-
-					// ** File Lock Handling **
-
-					struct flock savelock, unlock;
-
-					savelock.l_type = F_WRLCK;
-					savelock.l_whence = SEEK_SET;
-					savelock.l_start = offset + write_offset;
-					savelock.l_len = count;
-
-					unlock.l_type = F_UNLCK;
-					unlock.l_whence = SEEK_SET;
-					unlock.l_start = offset + write_offset;
-					unlock.l_len = count;
-
-					// Lock file segment that will be modified
-					int x = fcntl (fd, F_SETLK, &savelock);
-
-					if (x == -1) {
-
-						resp.number = -1;
-						message = "File is locked";
-						strcpy (resp.message, message);
-
-						write (newSocket, &resp, sizeof(resp));
-
-					}
-
-					// Perform write
-					lseek (fd, offset + write_offset, SEEK_SET);
-					int bytes = write (fd, buff, count);
-
-					char c;
-					scanf ("%c", &c);
-
-					// Unlock file segment that was modified
-					int y = fcntl (fd, F_SETLK, &unlock);
-
-					if (y == -1) {
-
-						resp.number = -1;
-						message = "Could not unlock file";
-						strcpy (resp.message, message);
-
-						write (newSocket, &resp, sizeof(resp));
-
-					} else {
-
-						resp.number = bytes;
-						message = "Success";
-						strcpy (resp.message, message);
-
-						write (newSocket, &resp, sizeof(resp));
-
-					}
-
-					// Free buffer and close file descriptor
-					free (buff);
-					close (fd);
-
-					break;
 
 				}
 
